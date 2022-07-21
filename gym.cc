b5b21ea910ce3973243008182db66d47335a7002
Alston D'mello <31090569+als10@users.noreply.github.com>
2022-07-21 15:35:18 -0400

    add property, academic building, residence, gym, player header

--- gym.cc
+++ gym.cc
@@ -8,5 +8,8 @@ class Gym: public Property {
     : name{name}, purchaseCost{purchaseCost}, propertyBlock{propertyBlock}, ownedBy{nullptr} {
     this->tuition[0] = tuition;
   }
+  int getTuition() const override {
+    return tuition[0];
+  }
   void visit(Player &player);
 };
